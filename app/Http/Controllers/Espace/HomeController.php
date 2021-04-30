8788706860428698e0c8d3b1b579a9b2c9e4b642
Alegne <lensalegne@gmail.com>
2021-04-30 08:48:42 +0300

    deploy v9

--- app/Http/Controllers/Espace/HomeController.php
+++ app/Http/Controllers/Espace/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        dd('die');
+        # dd('die');
 
         $data = null;
         $type = null;
